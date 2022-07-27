0054b33d4f3d4e3dc3f9517090bae8fccaf261b5
BISHAL <95861210+bsal1@users.noreply.github.com>
2022-07-27 03:17:08 +0000

    initial commit

---
+++ loading_animate.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+while true;
+do
+    printf "\r<  Loading     >" 
+    sleep 0.4
+    printf "\r>  Loading...  <" 
+    sleep 0.4 
+done
+#The printf statement uses the \r escape character to reset the terminal cursor of the current line. 
