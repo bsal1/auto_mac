6bc15e5bcc3b20c881a66aea7d02c2e12c1512d7
BISHAL <95861210+bsal1@users.noreply.github.com>
2022-09-17 06:03:24 +0000

    Add files via upload
    Number System Conversion

---
+++ ch_num_sys.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+while true 
+do
+    echo " Your input format :: 'number'  'its_base_value'  'conversion_base_value'"
+    read -p "Enter here :: " user_in user_base final_base
+    #This is the single line command in terminal.
+    echo " ibase=$user_base; obase=$final_base; $user_in" | bc
+    read -p "Do you want to continue? <y/N> " b
+    if [[ $b == "y" || $b == "Y" || $b == "yes" || $b == "Yes" || $b == "YES"  ]]
+    then
+        continue
+    else
+        exit 
+    fi
+done
