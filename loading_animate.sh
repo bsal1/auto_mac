c0720724ea6ffae34d4ad06cd335e3046d872440
BISHAL <95861210+bsal1@users.noreply.github.com>
2023-05-27 21:02:46 +0545

    Loading Animation

--- loading_animate.sh
+++ loading_animate.sh
@@ -3,7 +3,7 @@ while true;
 do
     printf "\r<  Loading     >" 
     sleep 0.4
-    printf "\r>  Loading...  <" 
+    printf "\r>  Loading...  <"  
     sleep 0.4 
 done
 #The printf statement uses the \r escape character to reset the terminal cursor of the current line. 
