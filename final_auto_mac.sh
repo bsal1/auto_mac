ed95227d95f48102f0c3286e03d794403694e35f
BISHAL <95861210+bsal1@users.noreply.github.com>
2023-05-27 21:01:39 +0545

    Mac-changer Automated

--- final_auto_mac.sh
+++ final_auto_mac.sh
@@ -4,7 +4,7 @@ if ( ip route | grep -q "default" )
   c=$(route | grep default | cut -d " " -f 37)
   echo "You are connected via $c."
   sudo ifconfig $c down
-  sudo macchanger -a $c
+  sudo macchanger -a $c 
   sudo ifconfig $c up
 else
   echo "Not connected to Internet"
