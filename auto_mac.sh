b9ec3a72f5a489233c16199733cfb59a67a879a8
BISHAL <95861210+bsal1@users.noreply.github.com>
2023-05-27 21:04:08 +0545

    Automatic Mac-changer

--- auto_mac.sh
+++ auto_mac.sh
@@ -4,7 +4,7 @@ if ( ip route | grep -q "default" )
   ven=$(macchanger -l | shuf -n 1 | cut -d " " -f 3 )  
   user=$(printf "%02x:%02x:%02x" $[RANDOM%255] $[RANDOM%255] $[RANDOM%255])
   c=$(route | grep default | cut -d " " -f 37)
-  echo "You are connected via $c."
+  echo "You are connected via $c." 
   sudo ifconfig $c down
   sudo macchanger -m "$ven:$user" $c
   sudo ifconfig $c up
